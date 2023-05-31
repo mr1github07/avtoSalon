9ec668795e8bb6190064acda662b0918f8625232
Asliddin18 <122342661+Asliddin18@users.noreply.github.com>
2023-05-31 20:35:54 +0500

    12
    12

--- app/pages/js/About.tsx
+++ app/pages/js/About.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import Navbar from "./Navbar";
 import "../css/About.css";
@@ -6,6 +7,12 @@ import office from "../images/office.jpg";
 import us from "../images/us.png";
 import gr from "../images/gr.jpg";
 import Team from "./Team";
+import img1 from "../images/galery.jpg";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 export default function About() {
   return (
@@ -54,11 +61,168 @@ export default function About() {
         </div>
         <Image src={gr} alt="" className="seo_img" />
         <div className="seo_center">
-            <h3>Book a Test Drive!</h3>
-            <button className="center_btn">Contact Us</button>
+          <h3>Book a Test Drive!</h3>
+          <button className="center_btn">Contact Us</button>
         </div>
       </div>
       <Team />
+      <div className="about_images">
+        <div className="image_left">
+          <Image src={img1} alt="" className="about_img1" />
+          <div className="img_box">
+            <Image src={img1} alt="" className="about_img2" />
+            <Image src={img1} alt="" className="about_img2" />
+          </div>
+        </div>
+        <div className="image_right">
+          <div className="img_box">
+            <Image src={img1} alt="" className="about_img2" />
+            <Image src={img1} alt="" className="about_img2" />
+          </div>
+          <div className="img_box">
+            <Image src={img1} alt="" className="about_img2" />
+            <Image src={img1} alt="" className="about_img2" />
+          </div>
+        </div>
+      </div>
+      <div className="question">
+        <h1>Frequently Asked Questions</h1>
+        <div className="question_body">
+          <div className="question_left">
+            <Accordion>
+              <AccordionSummary
+                id="panel1a-header"
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel2a-content"
+              >
+                <Typography>Accordion 1</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+                  eget.
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel2a-content"
+                id="panel2a-header"
+              >
+                <Typography>Accordion 2</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+                  eget.
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel2a-content"
+                id="panel2a-header"
+              >
+                <Typography>Accordion 2</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+                  eget.
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel2a-content"
+                id="panel2a-header"
+              >
+                <Typography>Accordion 2</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+                  eget.
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+          </div>
+          <div className="question_right">
+            <Accordion>
+              <AccordionSummary
+                id="panel1a-header"
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel2a-content"
+              >
+                <Typography>Accordion 1</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+                  eget.
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel2a-content"
+                id="panel2a-header"
+              >
+                <Typography>Accordion 2</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+                  eget.
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel2a-content"
+                id="panel2a-header"
+              >
+                <Typography>Accordion 2</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+                  eget.
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel2a-content"
+                id="panel2a-header"
+              >
+                <Typography>Accordion 2</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+                  eget.
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+          </div>
+        </div>
+        <button className="question_btn">Learn More</button>
+      </div>
     </div>
   );
 }
--- app/pages/js/Team.tsx
+++ app/pages/js/Team.tsx
@@ -15,29 +15,40 @@ export default function Team() {
     <div className="team">
       <div className="team_left">
         <h1>Our Team</h1>
-        <div className="team_box">
-          <BsCheckCircleFill />
-          <p>Praesent nibh luctus viverra</p>
-        </div>
-        <div className="team_box">
-          <BsCheckCircleFill />
-          <p>Adipsing elit</p>
-        </div>
-        <div className="team_box">
-          <BsCheckCircleFill />
-          <p>Praesent nibh luctus viverra</p>
-        </div>
-        <div className="team_box">
-          <BsCheckCircleFill />
-          <p>Praesent nibh luctus viverra</p>
-        </div>
-        <div className="team_box">
-          <BsCheckCircleFill />
-          <p>Lorem, ipsum.</p>
+        <div className="left_body">
+          <div className="left_box2">
+            <div className="team_box">
+              <BsCheckCircleFill />
+              <p>Praesent nibh luctus viverra</p>
+            </div>
+            <div className="team_box">
+              <BsCheckCircleFill />
+              <p>Adipsing elit</p>
+            </div>
+            <div className="team_box">
+              <BsCheckCircleFill />
+              <p>Praesent nibh luctus viverra</p>
+            </div>
+          </div>
+          <div className="left_box1">
+            <div className="team_box">
+              <BsCheckCircleFill />
+              <p>Praesent nibh luctus viverra</p>
+            </div>
+            <div className="team_box">
+              <BsCheckCircleFill />
+              <p>Lorem, ipsum.</p>
+            </div>
+          </div>
         </div>
         <button className="team_btn">Learn More</button>
       </div>
-      <Swiper slidesPerView={3} spaceBetween={30} className="mySwiper">
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        loop={true}
+        className="mySwiper"
+      >
         <SwiperSlide className="team_card">
           <Image src={cardimg} alt="" />
           <div className="Tcard_box">
@@ -84,34 +95,34 @@ export default function Team() {
           </div>
         </SwiperSlide>
         <SwiperSlide className="team_card">
-            <Image src={cardimg} alt="" />
-            <div className="Tcard_box">
-              <div className="tel_box"></div>
-              <div className="email_box"></div>
-              <h2>George Brown</h2>
-              <p>Customer Advisor</p>
-              <a className="Tcard_email" href="#!">
-                george@vehica.com
-              </a>
-              <a className="Tcard_tel" href="tel: +998931513776">
-                (123) 345-6789
-              </a>
+          <Image src={cardimg} alt="" />
+          <div className="Tcard_box">
+            <div className="tel_box"></div>
+            <div className="email_box"></div>
+            <h2>George Brown</h2>
+            <p>Customer Advisor</p>
+            <a className="Tcard_email" href="#!">
+              george@vehica.com
+            </a>
+            <a className="Tcard_tel" href="tel: +998931513776">
+              (123) 345-6789
+            </a>
           </div>
         </SwiperSlide>
 
         <SwiperSlide className="team_card">
-            <Image src={cardimg} alt="" />
-            <div className="Tcard_box">
-              <div className="tel_box"></div>
-              <div className="email_box"></div>
-              <h2>George Brown</h2>
-              <p>Customer Advisor</p>
-              <a className="Tcard_email" href="#!">
-                george@vehica.com
-              </a>
-              <a className="Tcard_tel" href="tel: +998931513776">
-                (123) 345-6789
-              </a>
+          <Image src={cardimg} alt="" />
+          <div className="Tcard_box">
+            <div className="tel_box"></div>
+            <div className="email_box"></div>
+            <h2>George Brown</h2>
+            <p>Customer Advisor</p>
+            <a className="Tcard_email" href="#!">
+              george@vehica.com
+            </a>
+            <a className="Tcard_tel" href="tel: +998931513776">
+              (123) 345-6789
+            </a>
           </div>
         </SwiperSlide>
       </Swiper>
