879a067623a1e9c2618d17376392d261acf5ce81
odilke <alautodil009@gmail.com>
2023-06-04 18:34:59 +0500

    авы
    авы

---
+++ app/login/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Footer from '../pages/js/Footer'
+import Login from '../pages/js/Login'
+
+export default function page() {
+  return (
+    <div>
+      <Login />
+      <Footer />
+    </div>
+  )
+}
